9b6ca0e02716d79bb03bc6ef924bc4ddfcf1d7a3
Jun Li <z5371930@ad.unsw.edu.au>
2023-09-25 22:55:23 +1000

    v1.0.1 color fix for tech stack

--- src/components/tech_stack.tsx
+++ src/components/tech_stack.tsx
@@ -23,9 +23,9 @@ const TechStack = () => {
                 <h2 className="font-bold text-2xl text-center">My Tech Stack 🦉</h2>
                 <h4 className="text-xl text-center">See my skill set!</h4>
                 <ul className="w-full md:w-35 tabs tabs-boxed flex flex-col items-center md:flex-row md:justify-center bg-gray-700">
-                    <li><a className="tab w-30 tab-active" id="lang" onClick={() => {selectDisplay("lang")}}>Languages</a></li>
-                    <li><a className="tab w-30" id="frame" onClick={() => {selectDisplay("frame")}}>Frameworks</a></li>
-                    <li><a className="tab w-30 sm:px-8" id="tool" onClick={() => {selectDisplay("tool")}}>Tools</a></li>
+                    <li><a className="tab w-30 tab-active text-blue-300 hover:text-blue-100" id="lang" onClick={() => {selectDisplay("lang")}}>Languages</a></li>
+                    <li><a className="tab w-30 text-blue-300 hover:text-blue-100" id="frame" onClick={() => {selectDisplay("frame")}}>Frameworks</a></li>
+                    <li><a className="tab w-30 sm:px-8 text-blue-300 hover:text-blue-100" id="tool" onClick={() => {selectDisplay("tool")}}>Tools</a></li>
                 </ul>
             </div>
             {display === "lang" ? <LangCollection /> : <></>}
